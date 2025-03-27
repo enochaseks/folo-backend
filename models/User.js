8120450c4134cbc77a25a0d4396b73485dda2ef8
enochaseks <155002633+enochaseks@users.noreply.github.com>
2025-03-27 23:06:28 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,65 @@
-// models/User.js
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { 
+      type: DataTypes.STRING, 
+      allowNull: false, 
+      unique: true,
+      validate: { isEmail: true }
+    },
     password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.ENUM("buyer", "seller"), defaultValue: "buyer" },
+    role: { 
+      type: DataTypes.ENUM("buyer", "seller"), 
+      defaultValue: "buyer" 
+    },
     githubId: { type: DataTypes.STRING, unique: true },
-    verificationToken: { type: DataTypes.STRING, allowNull: true },
-    isVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
-    resetToken: { type: DataTypes.STRING, allowNull: true },
-    resetTokenExpiry: { type: DataTypes.DATE, allowNull: true },
-  }, { timestamps: true });
+    verificationToken: { type: DataTypes.STRING },
+    isVerified: { 
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false 
+    },
+    resetToken: { type: DataTypes.STRING },
+    resetTokenExpiry: { type: DataTypes.DATE },
+    tokenVersion: { 
+      type: DataTypes.INTEGER, 
+      defaultValue: 0 
+    },
+    deletionScheduled: { 
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false 
+    },
+    deletionDate: { type: DataTypes.DATE }
+  }, { 
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    },
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    },
+    instanceMethods: {
+      validPassword: async function(password) {
+        return await bcrypt.compare(password, this.password);
+      }
+    }
+  });
+ 
 
   return User;
 };
